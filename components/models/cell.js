6e94d3218046095226e91a9e0197cd4880cdb98a
niu11g <2633340019@qq.com>
2019-12-31 23:46:42 +0800

    创建enum 动态设计样式

--- components/models/cell.js
+++ components/models/cell.js
@@ -1,8 +1,11 @@
+import {CellStatus} from "../../core/enum";
 
 
 class Cell{
     title
     id
+    status = CellStatus.WAITING
+
     constructor(spec) {
         this.title = spec.value
         this.id = spec.value_id
