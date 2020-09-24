5126b373783b825dc46698f96b49a94e52e4fec2
niu11g <2633340019@qq.com>
2020-09-24 15:34:01 +0800

    20200924 segment组件的应用 不同机型px与rpx动态转换概念 自适应高宽

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,4 +1,7 @@
 // pages/category/category.js
+import {getSystemSize} from "../../utils/system";
+import {px2rpx} from "../../miniprogram_npm/lin-ui/utils/util";
+
 Page({
 
   /**
@@ -11,8 +14,14 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: async function (options) {
+    const res = await getSystemSize()
+    const windowHeightRpx = px2rpx(res.windowHeight)
+    const h = windowHeightRpx - 60 -20 -2
+    console.log("segHeight:"+h)
+    this.setData({
+      segHeight:h
+    })
   },
 
   onGotoSearch(event){
